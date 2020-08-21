6da3b7fa930bb631cf6e7313791d156a1c611e21
Luca Sas <sas.luca.alex@gmail.com>
2020-08-21 02:39:18 +0100

    Updated graphics

--- devutils/amalgamate.bat
+++ devutils/amalgamate.bat
@@ -1,8 +1,17 @@
 @echo off
 
 echo Generating rayfork.h and rayfork.c
-amalgamate ../include/rayfork.h rayfork.h -i ../include/
-amalgamate ../src/rayfork.c rayfork.c -i ../src/ -i ../libs/
+
+:: This ensures the script gets executed from the dir its in
+pushd %~dp0
+pushd ..
+
+if not exist "rayfork-amalgamated" mkdir "rayfork-amalgamated"
+devutils\amalgamate include\rayfork.h rayfork-amalgamated\rayfork.h -i include\
+devutils\amalgamate src\rayfork.c     rayfork-amalgamated\rayfork.c -i src\ -i libs\
 
 :: >nul 2>&1 will silence the output in case the command is not present
-tar.exe -a -c -f rayfork.zip rayfork.h rayfork.c >nul 2>&1
+tar.exe -a -c -f rayfork-amalgamated\rayfork.zip rayfork-amalgamated\rayfork.h rayfork-amalgamated\rayfork.c >nul 2>&1
+
+popd
+popd
